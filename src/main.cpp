138e83331750e362b9d2df1de5fdf102aeb4331f
emilyhuang <emily.hby@gmail.com>
2020-01-11 17:16:25 -0500

    final version

--- src/main.cpp
+++ src/main.cpp
@@ -11,30 +11,39 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	string command, line;
 	char letter;
+	//2 vectors: one for original one for desired
 	vector<char> original;
 	vector<char> desired;
-	int index1 = 0, index2 = 0;
+	//read from the input file
 	fstream file(argv[1]);
+
+	//check the input file
 	if (!file.is_open()) cout<< "Could not open file\n";
-	//while(!file.eof()){
 	//cout << "old" << endl;
+
+	//make 1st vector
+	//continue to looop if it's not end of file or next character is a part of the alphabet
 	while(((file.peek() >= 65 && file.peek()<=90) || (file.peek() >= 97 && file.peek()<=122 ))&& !file.eof()){
 		file >> letter; 
-		cout << "inserting bookauthor: " << letter << endl;
+		//cout << "letter: " << letter << endl;
 		original.push_back(letter);
 		if(file.peek() == '\n') break;
 	}
-	cout << "new" << endl;
+
+	//make 2nd vector
+	//cout << "new" << endl;
 	while(!file.eof()){
 		file >> letter;
-		cout << "inserting bookauthor: " << letter << endl;
+		//cout << "letter: " << letter << endl;
 		desired.push_back(letter);
 		if(file.peek() == '\n') break;
 	}
+
+	//make a maxtrix using evenshtein distance method
 	vector<vector<int> > newmatrix = matrix(original, desired);	
 	string commandString = path(newmatrix, original, desired);
+	//make an output string for the moves needed
 	ofstream outputFile(argv[2]);
-    //outputFile.open(argv[2]);
     outputFile << commandString;
     outputFile.close();
 }
